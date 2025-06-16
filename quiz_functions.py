b5f51c1eb1f087bd0fa54bf4e3bfabc084a49559
Zadok-Little <zzlittle@students.everettcc.edu>
2025-06-16 06:56:58 +0000

    in main:     - Created code to take csv and put them into lists to later graph total     results of all quiz takers.     - Started code to seperate user into groups to simplify questions and     answers.
    in quiz_functions:
        - created a def that will take in a question and a list of answers,
        then return the answer the user gives
    
    in quiz_questions:
        - made a couple question variables to seperate user into groups
    
    in total_results:
        - made file with test data results to test graphing and writing over
        the results in the csv. Will zero out everything once program is
        finalized.

---
+++ quiz_functions.py
@@ -0,0 +1,10 @@
+
+
+def question_structure(question, answer_list):
+    print(question)
+    for x in answer_list:
+        print(x)
+
+    user_answer = str(input())
+
+    return user_answer
---
+++ quiz_questions.py
@@ -0,0 +1,3 @@
+first_group_questions = 'When going into combat are you (Answer with A or B):\nA: Charging into melee?\nB: Staying at range?\n'
+second_group_questions = 'In a fantasy setting, which would you use? (Answer with A or B):\nA: Physical Weapons\nB: Magical Prowess\n'
+
