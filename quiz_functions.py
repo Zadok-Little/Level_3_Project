182873b51091f70d3f51e9b8350ddca242e7d2a0
Zadok-Little <zzlittle@students.everettcc.edu>
2025-06-16 08:39:45 +0000

    in quiz_questions:     - formatted for future questions.
    in quiz_functions:
        - deleted old question format function and replaced it with functions
        to put user into a group, narrow user into a class, and roll stats
        for the user if desired.
    
    in main:
        - basically finished first draft of program. Working on questions to
        fill quiz_questions and finish the last function in quiz_functions.

--- quiz_functions.py
+++ quiz_functions.py
@@ -1,10 +1,48 @@
+from quiz_questions import *
+import random
+
+def class_result_function(questions):
+    results = []
+
+    return results
+
+def grouping_function():
+
+    group_answer = ''
+    totally_seperate_group_answer = ''
+    while not(group_answer == 'AA' or group_answer == 'AB' or group_answer == 'BA' or group_answer == 'BB'):
+
+        group_answer = str(input(first_group_questions).upper())
+        if not (group_answer == 'A' or group_answer == 'B'):
+            print('Invalid input, please try again:\n')
+            continue
 
 
-def question_structure(question, answer_list):
-    print(question)
-    for x in answer_list:
-        print(x)
+        while not(totally_seperate_group_answer == 'A' or totally_seperate_group_answer == 'B'):
 
-    user_answer = str(input())
+            totally_seperate_group_answer = str(input(second_group_questions).upper())
+            if not(totally_seperate_group_answer == 'A' or totally_seperate_group_answer == 'B'):
+                print('Invalid input, please try again:\n')
 
-    return user_answer
+        group_answer += totally_seperate_group_answer
+
+    return group_answer
+
+def roll_stats():
+    roll_character = str(input('Would you like for me to roll stats for you using 4d6 dropping the lowest roll? (Y / N)').upper())
+    while not(roll_character == 'Y' or roll_character == 'N'):
+        print('Invalid input, please try again:\n')
+        roll_character = str(input('Would you like for me to roll stats for you using 4d6 dropping the lowest roll? (Y / N)').upper())
+
+    if (roll_character == 'Y'):
+        for x in range(6):
+            roll_1 = random.randint(1,6)
+            roll_2 = random.randint(1,6)
+            roll_3 = random.randint(1,6)
+            roll_4 = random.randint(1,6)
+
+            rolls = [roll_1, roll_2, roll_3, roll_4]
+            smallest = min(rolls)
+            rolls.remove(smallest)
+
+            print(sum(rolls))
--- quiz_questions.py
+++ quiz_questions.py
@@ -1,3 +1,59 @@
 first_group_questions = 'When going into combat are you (Answer with A or B):\nA: Charging into melee?\nB: Staying at range?\n'
 second_group_questions = 'In a fantasy setting, which would you use? (Answer with A or B):\nA: Physical Weapons\nB: Magical Prowess\n'
 
+physical_melee_questions = [
+    {
+        'question': '', 
+        'options': {
+            'A': '',
+            'B': '',
+        },
+        'class_map': {
+            'A': '',
+            'B': '',
+        }
+
+    }
+]
+physical_range_questions = [
+    {
+        'question': '', 
+        'options': {
+            'A': '',
+            'B': '',
+        },
+        'class_map': {
+            'A': '',
+            'B': '',
+        }
+
+    }
+]
+magical_melee_questions = [
+    {
+        'question': '', 
+        'options': {
+            'A': '',
+            'B': '',
+        },
+        'class_map': {
+            'A': '',
+            'B': '',
+        }
+
+    }
+]
+magical_range_questions = [
+    {
+        'question': '', 
+        'options': {
+            'A': '',
+            'B': '',
+        },
+        'class_map': {
+            'A': '',
+            'B': '',
+        }
+
+    }
+]
